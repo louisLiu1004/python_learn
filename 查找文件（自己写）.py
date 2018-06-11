30eeb44ee5ed493286f0981cc37910340d44f231
louisFyt <lgc602598859@gmail.com>
2018-06-12 01:44:19 +0800

    查找文件并打开

--- 查找文件（自己写）.py
+++ 查找文件（自己写）.py
@@ -9,27 +9,27 @@ class Find_file():
     def run(self,path , key):
         try:
             files = os.listdir(path)
-            nums = 0
             for file_names in files:
                 new_full_path = os.path.join(path,file_names)
                 if os.path.isdir(new_full_path) :
-                    Find_file.run((new_full_path , key))
+                    Find_file().run(new_full_path , key)
                 elif os.path.isfile(new_full_path):
-                    print(nums ,'.',new_full_path)
-                    nums += 1
-                    search_files.append(new_full_path)
-            Find_file().open_files(input('想要打开哪个文件，输入序号（回车默认不打开任何文件）：'))
+                    if key.lower() in file_names.lower():
+                        print(len(search_files),'.',new_full_path)
+                        search_files.append(new_full_path)
         except Exception as Error_Infor:
             print(Error_Infor)
-
+# Find_file().open_files(input('想要打开哪个文件，输入序号（回车默认不打开任何文件）：'))
     def open_files(self,nums):
         if len(nums) == 0:
             pass
         else:
             os.system(search_files[int(nums)])
 
-
-Find_file().run(input('请输入路径：'),input('请输入关键词：'))
+s_dir = input('请输入路径：')
+s_key = input('请输入关键词：')
+Find_file().run(s_dir,s_key)
+Find_file().open_files(input('想要打开哪个文件，输入序号（回车默认不打开任何文件）：'))
 
 
 
