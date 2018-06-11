aa835f04bb8e87013cc0694b1f21581f1f28feed
louisFyt <lgc602598859@gmail.com>
2018-06-12 00:11:32 +0800

    查找文件并打开

--- 查找文件（自己写）.py
+++ 查找文件（自己写）.py
@@ -1,22 +1,39 @@
 __author__ = '財'
 
 
+
 import os
-import  threading
+global search_files
+search_files = []
+class Find_file():
+    def run(self,path , key):
+        try:
+            files = os.listdir(path)
+            nums = 0
+            for file_names in files:
+                new_full_path = os.path.join(path,file_names)
+                if os.path.isdir(new_full_path) :
+                    Find_file.run((new_full_path , key))
+                elif os.path.isfile(new_full_path):
+                    print(nums ,'.',new_full_path)
+                    nums += 1
+                    search_files.append(new_full_path)
+            Find_file().open_files(input('想要打开哪个文件，输入序号（回车默认不打开任何文件）：'))
+        except Exception as Error_Infor:
+            print(Error_Infor)
 
-def Find_file (path , key ):
-    try:
-        files = os.listdir(path)
-        for file_names in files:
-
-            new_full_path = os.path.join(path,file_names)
-            if os.path.isdir(new_full_path) :
-                Find_file(new_full_path,key)
-            elif os.path.isfile(new_full_path):
-                if key.lower() in file_names.lower():
-                    print(new_full_path)
-    except Exception as Error_Infor:
-        print(Error_Infor)
+    def open_files(self,nums):
+        if len(nums) == 0:
+            pass
+        else:
+            os.system(search_files[int(nums)])
 
 
-Find_file(input('PATH:'),input('KEY:'))
+Find_file().run(input('请输入路径：'),input('请输入关键词：'))
+
+
+
+
+
+
+
