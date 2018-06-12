cfb4bd7f17fa0a877b0d1b44bab3125d5b418097
LouisFyt <lgc602598859.gmail.com>
2018-06-12 18:08:32 +0800

    查找文件并打开

--- Find_file.py
+++ Find_file.py
@@ -1,26 +1,28 @@
 __author__ = '財'
 
-
-
 import os
 import subprocess
+# 导入模块
+
 global search_files
 search_files = []
+# 定义变量
 class Find_file():
-    def run(self,path , key):
+    def run(self, path, key):
         try:
-            files = os.listdir(path)
+            files = os.listdir(path) #列出路径内所有文件名
             for file_names in files:
-                new_full_path = os.path.join(path,file_names)
-                if os.path.isdir(new_full_path) :
-                    Find_file().run(new_full_path , key)
-                elif os.path.isfile(new_full_path):
-                    if key.lower() in file_names.lower():
-                        print('【',len(search_files),'】',' ',new_full_path,'\n')
+                new_full_path = os.path.join(path, file_names)
+                if os.path.isdir(new_full_path):  #判断是否是文件夹
+                    Find_file().run(new_full_path, key)
+                elif os.path.isfile(new_full_path):  #判断是否是文件
+                    if key.lower() in file_names.lower():  #判断是否包含关键字
+                        print('【', len(search_files), '】', ' ', new_full_path, '\n')
                         search_files.append(new_full_path)
         except Exception as Error_Infor:
             print(Error_Infor)
-    def open_files(self,nums):
+
+    def open_files(self, nums):
         if len(nums) == 0:
             pass
         else:
@@ -35,7 +37,7 @@ while True:
         Find_file().open_files(input('想要打开哪个文件，输入序号（回车默认不打开任何文件）：'))
     else:
         print('==========【没找到相关文件】=========')
-        s_key =input('请重新输入关键词：')
+        s_key = input('请重新输入关键词：')
         Find_file().run(s_dir,s_key)
 
 
