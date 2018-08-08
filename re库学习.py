96df71e697a94beafd8f76a93c8fa292b9b30fcc
louisFyt <lgc602598859@gmail.com>
2018-08-08 11:43:14 +0800

    爬虫入门学习

--- re库学习.py
+++ re库学习.py
@@ -55,27 +55,45 @@
 
 # 实战案例
 
-import requests
-import re
-import time
+# import requests
+# import re
+# import time
+#
+# def crew_data(pattern,rep):
+#     results = re.findall(pattern, rep)
+#     return results
+# def _main():
+#     print(time.ctime())
+#     rep = requests.get('https://book.douban.com').text
+#     pattern = re.compile(
+#         '<div\sclass="title">.*?href="(.*?)".*?title="(.*?)".*?more-meta.*?author">(.*?)</span>.*?year">(.*?)</span>.*?publisher">(.*?)</span>',
+#         re.S)
+#     results = crew_data(pattern,rep)
+#     for result in results:
+#         url, name, author, year, publisher = result
+#         name = re.sub('\s', '', name)
+#         author = re.sub('\s', '', author)
+#         year = re.sub('\s', '', year)
+#         publisher = re.sub('\s', '', publisher)
+#         print(url, '书名:', name, '作者:', author, '发行日期:', year, '出版社:', publisher)
+#     print(time.ctime())
+# if __name__ == '__main__':
+#     _main()
 
-def crew_data(pattern,rep):
-    results = re.findall(pattern, rep)
-    return results
-def _main():
-    print(time.ctime())
-    rep = requests.get('https://book.douban.com').text
-    pattern = re.compile(
-        '<div\sclass="title">.*?href="(.*?)".*?title="(.*?)".*?more-meta.*?author">(.*?)</span>.*?year">(.*?)</span>.*?publisher">(.*?)</span>',
-        re.S)
-    results = crew_data(pattern,rep)
-    for result in results:
-        url, name, author, year, publisher = result
-        name = re.sub('\s', '', name)
-        author = re.sub('\s', '', author)
-        year = re.sub('\s', '', year)
-        publisher = re.sub('\s', '', publisher)
-        print(url, '书名:', name, '作者:', author, '发行日期:', year, '出版社:', publisher)
-    print(time.ctime())
-if __name__ == '__main__':
-    _main()
+
+
+# 实战案例
+# import requests
+# import re
+# rep = requests.get('http://www.cbooo.cn/').text
+# parrent = re.compile('<td\sstyle.*?150px\'>(.*?)</td>.*?120px\'>(.*?)</td>.*?120px\'>(.*?)</td>.*?70px;\'>(.*?)</td>.*?</tr>',re.S)
+# results = re.findall(parrent,rep)
+# i = 1
+# for result in results:
+#     name,real,booking,days = result
+#     name = re.sub('\s','',name)
+#     real = re.sub('\s','',real)
+#     booking = re.sub('\s','',booking)
+#     days = re.sub('\s','',days)
+#     print('{0} \033[0;34;m影片名字：\033[0m {1} \033[0;32;m实时票房:\033[0m {2}万 \033[0;31;m累计票房:\033[0m {3}万 \033[0;33;m上映天数:\033[0m {4}'.format(i,name,real,booking,days))
+#     i+=1
